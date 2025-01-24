abb8e38b94a71222aa7d3f86cc309a33292ff2d0
gauravraj347 <127657698+gauravraj347@users.noreply.github.com>
2025-01-24 22:56:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./AuthProvider.jsx";
+import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
     <AuthProvider>
         <App />
+        <Footer/>
+        
     </AuthProvider>
 );
